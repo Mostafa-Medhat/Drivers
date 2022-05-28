746dea3741d80f0088e00aaf87a5fea1f442af57
Mostafa-Medhat <mmedhat72@gmail.com>
2022-05-28 02:28:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,37 +17,51 @@
 //#include "lm35_sensor.h"
 //#include "delay.h"
 //
-//#define LOGIC_LOW	0
-//#define LOGIC_HIGH	1
+//void ISR_ADC1(void);
+//
+//
 //int main(void)
 //{
-////	unsigned short int data=0;
-////	char flag;
+//
 //	unsigned char temperature;
 //
-////	GPIO_Init(GPIOB,PIN0_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN1_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN2_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN3_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN4_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN5_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN6_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
-////	GPIO_Init(GPIOB,PIN7_ID,PIN_OUTPUT,PUSH_PULL,RCC_GPIOB);
 //
 //
-//	GPIO_Init(GPIOA,PIN0_ID,ANALOG_MODE,NO_PULLUP_NO_PULLDOWN,RCC_GPIOA);
+//	GPIO_Init(GPIOA,ADC_CHANNEL_PIN_ID,ANALOG_MODE,NO_PULLUP_NO_PULLDOWN,RCC_GPIOA);
 //	ADCConfigType Config={bit12,single};
 //	ADC_Init(&Config);
+//
+//	GPIO_Init(GPIOA,PIN1_ID,PIN_INPUT,PULL_UP,RCC_GPIOA);
+//
 //	delayInit();
+//
 //	lcd_init();
 //	lcd_dispString("Hello,Mada7oota");
-////	delay_ms(2000);
 //	lcd_clrScreen();
 //	lcd_dispString("Temp =    C");	//Print "Temp =   C" on LCD
+//
+//	/*
+//	 * Get Initial Temperature
+//	 */
+//	ADC_StartConv(ADC_CHANNEL_PIN_ID);
+//	temperature=LM35_getTemperature();
+//
 //	while(1)
 //	{
+//		if(GPIO_ReadPin(GPIOA,PIN1_ID)==LOGIC_LOW)
+//		{
+//			delay_ms(5);
+//			if(GPIO_ReadPin(GPIOA,PIN1_ID)==LOGIC_LOW)
+//			{
+//					ADC_StartConv(ADC_CHANNEL_PIN_ID);
+//					temperature=LM35_getTemperature();
+//			}
+//		}
 //		lcd_gotoxy(0,7);
-//		temperature=LM35_getTemperature();
+//		if(Config.conversionMode==continuous)
+//		{
+//			temperature=LM35_getTemperature();
+//		}
 //		lcd_intgerToString(temperature);
 //		if(temperature<100)
 //		{
@@ -58,4 +72,13 @@
 //			}
 //		}
 //	}
+//}
+//
+//void ADC_IRQHandler(void)
+//{
+//	ISR_ADC1();
+//}
+//void ISR_ADC1(void)
+//{
+//	adc_value=ADC1->DR;
 //}
